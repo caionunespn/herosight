4a49e515449f5a707f87c2cc7549c277b91943cc
CaioNunes3000 <caionunes3000@gmail.com>
2019-08-14 01:17:27 -0300

    Updates

---
+++ precache-manifest.dd33a43b7a5e9f5a7685861515a4148b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c27499c4f7611ac5a47d236dcb9157",
+    "url": "/herosight/index.html"
+  },
+  {
+    "revision": "e5b9c1b78b6484d04415",
+    "url": "/herosight/static/css/main.9cdf0ac8.chunk.css"
+  },
+  {
+    "revision": "80a6d044eb84c505d298",
+    "url": "/herosight/static/js/2.057af5a5.chunk.js"
+  },
+  {
+    "revision": "e5b9c1b78b6484d04415",
+    "url": "/herosight/static/js/main.17971414.chunk.js"
+  },
+  {
+    "revision": "d651c8fb737e8dc1a07c",
+    "url": "/herosight/static/js/runtime~main.2fdd52a8.js"
+  },
+  {
+    "revision": "139f063815197859b79cb83afde22e35",
+    "url": "/herosight/static/media/logo.139f0638.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53b2f3dd600833c711b3eedc09880c56.js"
+  "/herosight/precache-manifest.dd33a43b7a5e9f5a7685861515a4148b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/herosight/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
