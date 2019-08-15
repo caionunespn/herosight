3bba716514e221aac9f2f0b99afa23a16330f91b
CaioNunes3000 <caionunes3000@gmail.com>
2019-08-15 17:31:42 -0300

    Adicionei a 'equipe'

--- src/pages/equipe.js
+++ src/pages/equipe.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import './equipe.css';
 export default function Equipe(){
     return(
-        <div>
-            <p>Aqui fica a equipe</p>
+        <div class='equipe-container'>
+            <div class='membro-card'>
+                <div class='membro-card-img'>
+                    <img src='https://avatars3.githubusercontent.com/u/20321685?v=4' alt='' />
+                </div>
+                <div class='membro-card-text'>
+                    <h1>Caio Nunes</h1>
+                    <p>Não tinha nada pra fazer daí resolvi criar esse app.</p>
+                </div>
+            </div>
         </div>
     );
 }
